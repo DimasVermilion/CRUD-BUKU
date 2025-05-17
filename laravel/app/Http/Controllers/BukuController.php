5df3552a08b5dd7793c3509a34e4a059f0c77553
NevidAmrullah <nevidamrullah24@gmail.com>
2025-05-17 14:24:17 +0700

    json in complete

--- laravel/app/Http/Controllers/BukuController.php
+++ laravel/app/Http/Controllers/BukuController.php
@@ -4,59 +4,141 @@ namespace App\Http\Controllers;
 use App\Models\Buku;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class BukuController extends Controller
 {
-     public function index()
-    {
-        //
+     public function index() {
+
+    $books = Buku::query()->latest()->get();
+    // $artikels = $query->paginate(10);
+
+    if ($books->isEmpty()) {
+        return response()->json([
+            'status'=> Response::HTTP_NOT_FOUND,
+            'massage'=>'artikel empty'
+        ],Response::HTTP_NOT_FOUND);
+        }else {
+            return response()->json([
+                'data'=> $books->map(function($book){
+                    return [
+                        'id' => $book->id,
+                        'title' => $book->title,
+                        'author' => $book->author,
+                        'page' => $book->page,
+                        'category' => $book->Category->category,
+                        'publish_date' => $book->publish_date
+                        ];
+                    }),
+
+                    'status'=> Response::HTTP_OK,
+                    'message'=>'list buku'
+                    ]);
+
+                }
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'title' => 'required',
+            'author' => 'required',
+            'page' => 'required',
+            'category_id' => 'required',
+            'publish_date' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+        else{
+            Buku::create([
+                'title' => $request->title,
+                'author' => $request->author,
+                'page' => $request->page,
+                'category_id' => $request->category_id,
+                'publish_date' => $request->publish_date
+            ]);
+
+            return response()->json([
+                'status' => Response::HTTP_OK, 'message' => 'data masuk db'
+            ],Response::HTTP_OK);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function show($id)//24-nevid
     {
-        //
+        //$Artikel = Artikel::where('id', $id)->first(); //-> ngambil data sesuai id
+
+        $book = Buku::find($id); //-> ngambil data sesuai id
+
+        if ($book) {                     //jika data ada  muncul kan datanya sesuai isi if
+            return response()->json([
+                'status' => Response::HTTP_OK,
+                'data' => [
+                'title' => $book->title,
+                'author' => $book->author,
+                'page' => $book->page,
+                'category' => $book->Category->name,
+                'publish_date' => $book->publish_date
+                ]
+            ], Response::HTTP_OK);
+        } else {
+            //jika tidak berarti tampilkan else (not found)
+            return response()->json([
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => 'Artikel not found'
+            ], Response::HTTP_NOT_FOUND);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Buku $buku)
+    public function update(Request $request, $id)//24-nevid
     {
-        //
+        $book = Buku::find($id);
+
+        if (!$book) {
+            return response()->json([
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => 'Artikel not found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'author' => 'required',
+            'page' => 'required',
+            'category_id' => 'required',
+            'publish_date' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }else {
+            $book->update([
+                'title' => $request->title,
+                'author' => $request->author,
+                'page' => $request->page,
+                'category_id' => $request->category_id,
+                'publish_date' => $request->publish_date
+            ]);
+
+            return response()->json([
+                'status' => Response::HTTP_OK,
+                'message' => 'Data updated'
+            ], Response::HTTP_OK);
+        }
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Buku $buku)
-    {
-        //
-    }
+    public function destroy($id){//24-nevid
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Buku $buku)
-    {
-        //
-    }
+        $book = Buku::find($id);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Buku $buku)
-    {
-        //
+        $book->delete();
+            return response()->json([
+                'status'=> Response::HTTP_OK,
+                'massage'=>'artikel dihapus'
+            ],Response::HTTP_OK);
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,10 +1,33 @@
 <?php
 
+use App\Http\Controllers\BukuController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('list-buku', [BukuController::class, 'index']);
+
+
+Route::post('store-buku', [BukuController::class, 'store']);
+
+
+Route::get('read-artikel/{id}',[BukuController::class,'show']);
+
+Route::put('update-artikel/{id}',[BukuController::class,'update']);
+
+
+Route::delete('delete-artikel/{id}',[BukuController::class,'destroy']);
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+});
+
 
 

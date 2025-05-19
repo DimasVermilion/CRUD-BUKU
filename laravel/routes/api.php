9c5ecc72aa77194f15e42acff47f4692df5f348b
NevidAmrullah <nevidamrullah24@gmail.com>
2025-05-19 17:33:55 +0700

    Authentifikasi

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BukuController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
@@ -21,9 +22,13 @@ Route::get('read-category/{id}',[CategoryController::class,'show']);
 Route::put('update-category/{id}',[CategoryController::class,'update']);
 Route::delete('delete-category/{id}',[CategoryController::class,'destroy']);
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
 
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:sanctum')->group(function () {
